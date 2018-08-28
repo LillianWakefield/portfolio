0a594168ed7ab552d51a622d3afd6f5353ecbeb3
Lillian Wakefield <lillianwakefield@gmail.com>
2018-08-28 18:56:36 -0500

    updated chislango

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 $.get('http://public-api.wordpress.com/rest/v1/sites/theoffseasoner.wordpress.com/posts', function(data){
             $('blog-container').attr('src', data.ID);
             console.log(data)
         });
-});     
+});     */
